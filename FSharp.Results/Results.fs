9012d3be15653354c928708955a6af8f6188c9e0
Oskar Gewalli <oskar@gewalli.se>
2016-07-08 23:24:12 +0200

    Cleanup

---
+++ FSharp.Results/Results.fs
@@ -0,0 +1,41 @@
+﻿namespace FSharp.Results
+
+module Results =
+
+  let ifNone (error : 'TError) = function
+    | Some (o) -> Ok o
+    | None -> Error error
+
+  let map f = function
+    | Ok x -> f x
+    | Error e -> Error e
+
+  let bind f m=
+    match m with
+    | Ok x -> f x
+    | Error e -> Error e
+
+  let tryResult (body, err) =
+    try
+      Ok <| body()
+    with e -> Error err
+
+  type ResultBuilder () =
+    member __.Zero () = Error
+    member __.Bind (m , f) = bind f m
+    member __.Return (v) = Ok v
+    member __.ReturnFrom (v) = v
+    member __.Delay (f) = fun () -> f ()
+    member __.Run (f) = f()
+    member __.TryWith (body, handler) =
+      try
+        __.Run body
+      with e -> handler e
+
+    member __.TryFinally (body, handler) =
+      try
+        __.Run body
+      finally
+        handler ()
+
+  let attempt = ResultBuilder()
--- Tests/ResultTests.fs
+++ Tests/ResultTests.fs
@@ -1,5 +1,36 @@
 ﻿namespace Tests
 
-type Class1() = 
-    member this.X = "F#"
 
+open System
+open NUnit.Framework
+
+type EmailValidation=
+    | Empty
+    | NoAt
+
+open FSharp.Results
+open Results
+
+[<TestFixture>]
+type ResultTests() =
+
+    let fail_if_empty email=
+        if String.IsNullOrEmpty(email) then Error Empty else Ok email
+
+    let fail_if_not_at (email:string)=
+        if (email.Contains("@")) then Ok email else Error NoAt
+
+    let validate_email =
+        fail_if_empty
+        >> bind fail_if_not_at
+
+    let test_validate_email email (expected:Result<string,EmailValidation>) =
+        let actual = validate_email email
+        Assert.AreEqual(expected, actual)
+
+
+    [<Test>]
+    member this.CanChainTogetherSuccessiveValidations() =
+        test_validate_email "" (Error Empty)
+        test_validate_email "something_else" (Error NoAt)
+        test_validate_email "some@email.com" (Ok "some@email.com")
